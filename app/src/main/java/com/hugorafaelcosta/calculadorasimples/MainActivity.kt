0dbba2c58ab45ff1706898baf166e2dc5d500932
Hugo Costa <costarafaelhugo@gmail.com>
2021-11-03 12:56:30 -0300

    Mudei o layout, retirei o RelativeLayout e coloquei apenas containers menores com LinearLayout, também adicionei dois campos de texto, um para a operação e o outro para a exibição da resposta. Também retirei o actionbar.

--- app/src/main/java/com/hugorafaelcosta/calculadorasimples/MainActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/calculadorasimples/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.hugorafaelcosta.calculadorasimples
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.hugorafaelcosta.calculadorasimples.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private  lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
     }
 }
+
+
