d51064a87519ef596df8acde83c3a1b9de2eeff0
Hugo Costa <costarafaelhugo@gmail.com>
2021-11-03 13:54:49 -0300

    Adicionei o evento de click nos numeros e agora eles aparecem na tela ao serem clicados

--- app/src/main/java/com/hugorafaelcosta/calculadorasimples/MainActivity.kt
+++ app/src/main/java/com/hugorafaelcosta/calculadorasimples/MainActivity.kt
@@ -8,14 +8,73 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        val numberZero = binding.btnNumber0.setOnClickListener {
+
+            addExpression("0", true)
+        }
+        val numberOne = binding.btnNumberOne.setOnClickListener {
+
+            addExpression("1", true)
+        }
+        val numberTwo = binding.btnNumberTwo.setOnClickListener {
+
+            addExpression("2", true)
+        }
+        val numberThree = binding.btnNumberThree.setOnClickListener {
+            addExpression("3", true)
+        }
+        val numberFour = binding.btnNumberFour.setOnClickListener {
+            addExpression("4", true)
+        }
+        val numberFive = binding.btnNumberFive.setOnClickListener {
+            addExpression("5", true)
+        }
+        val numberSix = binding.btnNumberSix.setOnClickListener {
+            addExpression("6", true)
+        }
+        val numberSeven = binding.btnNumberSeven.setOnClickListener {
+            addExpression("7", true)
+        }
+        val numberEight = binding.btnNumberEight.setOnClickListener {
+            addExpression("8", true)
+        }
+        val numberNine = binding.btnNumberNine.setOnClickListener {
+            addExpression("9", true)
+        }
+
+
         supportActionBar?.hide()
+
+
     }
+
+
+
+
+    fun addExpression(string: String, clear: Boolean) {
+        var result = binding.result
+        var expression = binding.expression
+
+        if (result.text.isNotEmpty()) {
+            expression.setText("")
+        }
+
+        if (clear) {
+            result.setText("")
+            expression.append(string)
+        } else {
+            expression.append(result.text)
+            expression.append(string)
+            result.setText("")
+        }
+    }
+
 }
 
 
